a6b1943d91d5b375aad5450d47670eba1eb021b1
Alex Glandon <aglan001@odu.edu>
2026-06-25 19:48:35 -0400

    moved nonlinear single test to single test folder

---
+++ nonlinear_least_squares_single_test/nonlinear_least_squares_window_1000/run_snr_20_distance_05_grid_6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --exclusive
+
+#SBATCH -J snr_20_distance_05_grid_6
+#SBATCH --output=out_snr_20_distance_05_grid_6.out
+
+enable_lmod
+module load container_env python3
+
+crun python -u nonlinear_least_squares.py 20 5 6
